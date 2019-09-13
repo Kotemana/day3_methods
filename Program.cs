fd5ea539c340d84be044971cdd49a13574b84b15
Leonid Radkevich <laradkevich@gmail.com>
2019-09-13 21:43:50 +0300

    changes

--- Program.cs
+++ Program.cs
@@ -7,28 +7,38 @@ namespace try4
         static void Main(string[] args)
         {
             string riddleMessage = "Who goes on morning on 4, on day on 2, on evening on 3";
-            Riddle(riddleMessage, "human");
+            Riddle(riddleMessage, "human", 4);
 
             string harderRiddle = "2 ends 2 rings and a nail in the middle";
-            Riddle(harderRiddle, "scissors");
+            Riddle(harderRiddle, "scissors", 2);
 
-            Riddle("Pear hangs, wtf?", "bulb");
+            Riddle("Pear hangs, wtf?", "bulb", 3);
 
             Console.WriteLine("all riddles solved");
             Console.ReadLine();
         }
 
-        static void Riddle(string riddleText, string rightAnswer)
+        static void Riddle(string riddleText, string rightAnswer, int attemps)
         {
             Console.WriteLine("Here is the riddle:");
             string answer="";
+            string isRight = "You are right, right answer is";
             while (answer != rightAnswer)
             {
+                
+                if (attemps == 0)
+                {
+                    isRight = "No more attempts, right answer was ";
+                    break;
+                }
+                Console.WriteLine($"You have {attemps} attempts left");
                 Console.WriteLine($"{riddleText}");
                 answer=Console.ReadLine();
+                attemps--;
             }
-            Console.WriteLine($"Right, it's {rightAnswer}");
+            Console.WriteLine($"{isRight} {rightAnswer}");
 
         }
+
     }
 }
